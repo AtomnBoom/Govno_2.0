9e77977545316798fd9c8b3b9741982b0b8eac38
AD <@GAMMA>
2026-06-15 21:06:19 +0300

    1

--- Govno_2.0/Models/Clients.cs
+++ Govno_2.0/Models/Clients.cs
@@ -22,7 +22,6 @@ namespace Govno_2._0.Models
         public int ID { get; set; }
         public string FullName { get; set; }
         public string Phone { get; set; }
-        public int Number { get; set; } 
 
         public virtual ICollection<Orders> Orders { get; set; }
     }
